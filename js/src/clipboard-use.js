1c54016cd911f81969bb61d70ea1397973ccd859
pyf <yongfeng_peng@163.com>
2022-03-28 15:06:55 +0800

    Site updated: 2022-03-28 15:06:54

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,18 @@
+/*页面载入完成后，创建复制按钮*/
+!function (e, t, a) { 
+  /* code */
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '  <i class="fa fa-globe"></i><span class="copy-txt">copy</span>';
+    copyHtml += '</button>';
+    $(".highlight .code pre").before(copyHtml);
+    new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            // $('.copy-txt').text('复制成功')
+            return trigger.nextElementSibling;
+        }
+    });
+  }
+  initCopyCode();
+}(window, document);
